5eaf411a019a1858a245153acb8d8de6e107734d
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-19 14:12:23 +0330

    Created simple intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hi, I'm Arsalan Mohseni</h1>
+<p>A fullstack webdeveloper that loves to build things.</p>
+<p>I'm currently working on a project called <a href="https://github.com/arsalanmohseni/sveltekit-blog">Sveltekit Blog</a> that is a blog built with SvelteKit and Tailwind CSS.</p>
+<p>I'm also a big fan of <a href="https://www.typescriptlang.org/">Typescript</a>, <a href="https://svelte.dev/">Svelte and Sveltekit</a>, <a href="https://tailwindcss.com/">Tailwind CSS</a> and <a href="https://nextjs.org/">React and Next JS</a>.</p>
+<p>If you want to contact me, you can find me on <a href="https://github.com/arsalanmohseni">Github</a></p>
