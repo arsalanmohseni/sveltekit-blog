c47c6d04f5e1768a6b11f5443765b1e4cc246d8f
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-22 21:51:34 +0330

    Added a lottie file as an intro

---
+++ src/lib/assets/lotties/Intro.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { DotLottieSvelte } from '@lottiefiles/dotlottie-svelte';
+</script>
+
+<div class="h-96 w-96">
+    <DotLottieSvelte
+    	src="https://lottie.host/aad79abe-35ce-482e-b876-27ddf7df1faf/vNcPtDYU7M.lottie"
+    	background="transparent"
+    	speed="1"
+    	loop
+    	autoplay
+    />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,33 @@
-<section class="text-center">
-	<h1>Hi, I'm Arsalan Mohseni</h1>
-	<p>A fullstack webdeveloper that loves to build things.</p>
-	<p>
-		I'm currently working on a project called <a
-			href="https://github.com/arsalanmohseni/sveltekit-blog">Sveltekit Blog</a
-		> that is a blog built with SvelteKit and Tailwind CSS.
-	</p>
-	<p>
-		I'm also a big fan of <a href="https://www.typescriptlang.org/">Typescript</a>,
-		<a href="https://svelte.dev/">Svelte and Sveltekit</a>,
-		<a href="https://tailwindcss.com/">Tailwind CSS</a>
-		and <a href="https://nextjs.org/">React and Next JS</a>.
-	</p>
-	<p>
-		If you want to contact me, you can find me on <a href="https://github.com/arsalanmohseni"
-			>Github</a
-		>
-	</p>
+<script>
+	import Intro from '../lib/assets/lotties/Intro.svelte';
+</script>
+
+<section class="text-left *:ml-5 *:text-2xl">
+	<div class="flex items-center">
+		<div class="flex flex-col">
+		    <h1>Hi, I'm Arsalan Mohseni</h1>
+    		<p>A fullstack web developer that loves to build things.</p>
+    		<p>
+    			I'm currently working on a project called <a
+    				href="https://github.com/arsalanmohseni/sveltekit-blog">Sveltekit Blog</a
+    			> that is a blog built with SvelteKit and Tailwind CSS.
+    		</p>
+    		<p>
+    			I'm also a big fan of <a href="https://www.typescriptlang.org/">Typescript</a>,
+    			<a href="https://svelte.dev/">Svelte and Sveltekit</a>,
+    			<a href="https://tailwindcss.com/">Tailwind CSS</a>
+    			and <a href="https://nextjs.org/">React and Next JS</a>.
+    		</p>
+    		<p>
+    			If you want to contact me, you can find me on <a
+    				href="https://github.com/arsalanmohseni">Github</a
+    			>
+    		</p>
+		</div>
+		<Intro />
+	</div>
 </section>
 
-<section class="mt-10">
-	<h3>The most recent posts</h3>
-    
+<section class="ml-5 mt-10">
+	<h3 class="ml-5 text-center text-4xl font-black">The most recent posts</h3>
 </section>
