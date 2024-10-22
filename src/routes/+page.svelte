cc945c42b16b8bf62e33633d71c8318b245e7c41
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-22 20:54:43 +0330

    Linted codes and added new text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
-<h1>Hi, I'm Arsalan Mohseni</h1>
-<p>A fullstack webdeveloper that loves to build things.</p>
-<p>I'm currently working on a project called <a href="https://github.com/arsalanmohseni/sveltekit-blog">Sveltekit Blog</a> that is a blog built with SvelteKit and Tailwind CSS.</p>
-<p>I'm also a big fan of <a href="https://www.typescriptlang.org/">Typescript</a>, <a href="https://svelte.dev/">Svelte and Sveltekit</a>, <a href="https://tailwindcss.com/">Tailwind CSS</a> and <a href="https://nextjs.org/">React and Next JS</a>.</p>
-<p>If you want to contact me, you can find me on <a href="https://github.com/arsalanmohseni">Github</a></p>
+<section class="text-center">
+	<h1>Hi, I'm Arsalan Mohseni</h1>
+	<p>A fullstack webdeveloper that loves to build things.</p>
+	<p>
+		I'm currently working on a project called <a
+			href="https://github.com/arsalanmohseni/sveltekit-blog">Sveltekit Blog</a
+		> that is a blog built with SvelteKit and Tailwind CSS.
+	</p>
+	<p>
+		I'm also a big fan of <a href="https://www.typescriptlang.org/">Typescript</a>,
+		<a href="https://svelte.dev/">Svelte and Sveltekit</a>,
+		<a href="https://tailwindcss.com/">Tailwind CSS</a>
+		and <a href="https://nextjs.org/">React and Next JS</a>.
+	</p>
+	<p>
+		If you want to contact me, you can find me on <a href="https://github.com/arsalanmohseni"
+			>Github</a
+		>
+	</p>
+</section>
+
+<section class="mt-10">
+	<h3>The most recent posts</h3>
+    
+</section>
